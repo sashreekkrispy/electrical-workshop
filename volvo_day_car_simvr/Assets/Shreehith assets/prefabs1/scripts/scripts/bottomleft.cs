7197ce2a515b8a44448c5f2cd722b3fc563fb4d0
sashreekkrispy <sashreek.uyyala@gmail.com>
2023-09-04 18:35:56 +0530

    dsds

---
+++ volvo_day_car_simvr/Assets/Shreehith assets/prefabs1/scripts/scripts/bottomleft.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bottomleft : MonoBehaviour
+{
+    public Animator object1animator;
+
+    public bool animator1 = false;
+    public destroycounter counter;
+    void Update()
+    {   
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (counter.trigger == true )
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (hit.collider.CompareTag("cuts1"))
+                    {
+                        if ((object1animator != null))
+                        {
+                            object1animator.SetTrigger("DestroyTrigger");
+                            Debug.Log("bottomleft");
+                            StartCoroutine(waitforseconds());
+                            animator1 = true;
+                        }
+
+                    }
+                }
+
+            }
+        }
+        
+        IEnumerator waitforseconds()
+        {
+            yield return new WaitForSeconds(3);
+            Destroy(gameObject);
+            destroycounter.currentdestroy++;
+            Debug.Log(destroycounter.currentdestroy);
+        }
+    }
+}
---
+++ volvo_day_car_simvr/Assets/Shreehith assets/prefabs1/scripts/scripts/bottomright.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bottomright : MonoBehaviour
+{
+    public Animator object2animator;
+
+    public bool animator2 = false;
+    public destroycounter counter;
+
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (counter.trigger == true)
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (hit.collider.CompareTag("cuts2"))
+                    {
+                        object2animator.SetTrigger("DestroyTrigger");
+                        Debug.Log("bottomright");
+                        StartCoroutine(waitforseconds());
+                        animator2 = true;
+
+                    }
+                }
+
+            }
+        }
+       
+        IEnumerator waitforseconds()
+        {
+            yield return new WaitForSeconds(3);
+            Destroy(gameObject);
+            destroycounter.currentdestroy++;
+            Debug.Log(destroycounter.currentdestroy);
+        }
+    }
+}
